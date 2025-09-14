d76e2f220770329d589549827dbea3ec138a0e55
AriesHoo <AriesHoo@126.com>
2025-09-14 22:44:40 +0800

    初始版本

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "iflytek_sparkchain"
+include(":app")
+ 
